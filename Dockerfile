3b214ff74c8a3cc2b2f21092dd1783c9f25463cb
vladuxa23 <admin@admin.com>
2023-07-07 14:16:58 +0300

    add docker files and some refactor

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM python:3.9-slim
 WORKDIR /usr/src/app
-RUN apt update && apt upgrade && apt install bash
+RUN apt update -y
+RUN apt upgrade -y
+RUN apt install -y bash
 RUN apt install -y libgl1
 RUN apt install -y tesseract-ocr
 RUN apt install -y libtesseract-dev
+RUN apt install -y poppler-utils
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 3040
 
 RUN ["python3", "-m", "pip", "install", "-r", "./requirements.txt"]
 CMD ["python3", "./manage.py"] 
--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import traceback
 import cv2
 import io
 import logging
@@ -11,12 +12,9 @@ import uvicorn
 from fastapi import FastAPI, File
 from fastapi.openapi.docs import get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html, get_redoc_html
 from fastapi.staticfiles import StaticFiles
-from starlette.requests import Request
-import pdfminer.high_level as pdfminerHL
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.layout import LAParams
-from pdfminer.converter import TextConverter
-from pdfminer.pdfpage import PDFPage
+from pdf2image import convert_from_bytes
+
+
 import io
 
 from utils.logger import logger
@@ -86,32 +84,38 @@ def read_img(img):
 
 @app.post("/interrogate_image")
 async def extract_image(file: bytes = File()):
-    image_stream = io.BytesIO(file)
-    image_stream.seek(0)
-    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
-    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    label = read_img(frame)
+    try:
+        image_stream = io.BytesIO(file)
+        image_stream.seek(0)
+        file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+        frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        label = read_img(frame)
 
-    return {"result": label}
+        return {"result": label}
+    except Exception:
+        logging.exception('Got exception on interrogate handler')
+
+        return {"result": "error interrogate"}
 
 
 @app.post("/interrogate_pdf")
 async def extract_pdf(file: bytes = File()):
-    text_memory_file = io.StringIO()
+    try:
+        pages = convert_from_bytes(file)
+        
+        text_data = ''
+        for page in pages:
+            text = pytesseract.image_to_string(page)
+            text_data += text + '\n'
+        
+        return {"result":text_data}
+    except Exception:
+        logging.exception('Got exception on interrogate handler')
 
-    rsrcmgr = PDFResourceManager()
-    device = TextConverter(rsrcmgr, text_memory_file, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    # get first 3 pages of the pdf file
-    
-    for page in PDFPage.get_pages(file, pagenos=(0, 1)):
-        interpreter.process_page(page)
-    text = text_memory_file.getvalue()
-    text_memory_file.close()
-    return text
+        return {"result": "error interrogate"}
 
 
 
 
 if __name__ == "__main__":
-    uvicorn.run("manage:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("manage:app", host="0.0.0.0", port=3040, reload=False)
